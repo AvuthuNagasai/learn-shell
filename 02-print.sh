4b86c0145fc6a97d1561d83c4d99386c95d49cb3
nagasai <avuthunagasaireddy@gmail.com>
2023-10-08 21:20:10 +0530

    printing comments in coloue

---
+++ 02-print.sh
@@ -0,0 +1,8 @@
+echo hello world
+
+echo -e "\e[31m Hello in red color\e[0m"
+echo -e "\e[32m Hello in green color\e[0m"
+echo -e "\e[33m Hello in blue color\e[0m"
+echo -e "\e[34m Hello in yellow color\e[0m"
+echo -e "\e[35m Hello in magenta color\e[0m"
+echo -e "\e[36m Hello in cyan color\e[0m"
